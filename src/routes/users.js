0246f62b931a674d036341ce3bdde0a344ded4a3
Leon Li <leonli369@gmail.com>
2020-12-21 22:55:08 -0500

    added the main routes for a user

---
+++ src/routes/users.js
@@ -0,0 +1,54 @@
+const auth = require("../middleware/auth");
+const { User, validate } = require("../models/user");
+const express = require("express");
+const router = express.Router();
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+
+router.get("/all", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+  res.send(user);
+});
+
+router.post("/add", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (user)
+    return res.status(400).send("Email is already registered with an account");
+
+  user = new User(_.pick(req.body, ["name", "email", "password", "isAdmin"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  user.save();
+
+  const token = user.generateAuthToken();
+  res
+    .header("x-auth-token", token)
+    .send(_.pick(user, ["name", "email", "id", "isAdmin"]));
+});
+
+router.patch("/update", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = _.pick(req.body, ["name", "email", "password"]);
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  const updatedUser = await User.findByIdAndUpdate(req.user._id, user, {
+    new: true,
+  });
+
+  if (!updatedUser)
+    return res.status(404).send("The account was not found in the db");
+
+  res.send(_.pick(updatedUser, "name", "email", "id", "isAdmin"));
+});
+
+module.exports = router;
